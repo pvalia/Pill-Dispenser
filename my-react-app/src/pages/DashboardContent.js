2774f8a2b3b82179ab6ed42080860e2df3892c94
pvalia <pvaliakhmetova@gmail.com>
2024-01-13 21:00:46 -0500

    web page

---
+++ my-react-app/src/pages/DashboardContent.js
@@ -0,0 +1,357 @@
+import "./DashboardContent.css";
+
+const DashboardContent = () => {
+  return (
+    <div className="dashboard-content">
+      <div className="dashboard-content-child" />
+      <div className="mainframe">
+        <div className="patients">
+          <div className="patients-child" />
+          <div className="patient-info">
+            <div className="patient-info-parent">
+              <div className="patient-info1">Patient Info</div>
+              <div className="namesection">
+                <img
+                  className="namesection-child"
+                  alt=""
+                  src="/rectangle-39905.svg"
+                />
+                <div className="name">Name</div>
+                <textarea className="namebox" placeholder="Name" />
+              </div>
+              <div className="agesection">
+                <div className="age">Age</div>
+                <textarea className="agebox" placeholder="#" />
+              </div>
+              <div className="gendersection">
+                <div className="gender">Gender</div>
+                <textarea className="genderbox" placeholder="M/F" />
+              </div>
+              <div className="emailsection">
+                <div className="email">Email</div>
+                <textarea className="emailbox" placeholder="email" />
+              </div>
+            </div>
+            <div className="update">Update</div>
+          </div>
+          <div className="consultation">
+            <div className="medical-history">Medical History</div>
+            <div className="rectangle-parent">
+              <div className="group-child" />
+              <div className="vector-parent">
+                <img className="group-item" alt="" src="/vector-4.svg" />
+                <div className="group-parent">
+                  <div className="group-wrapper">
+                    <div className="group-wrapper">
+                      <div className="group-inner" />
+                      <div className="ellipse-div" />
+                      <div className="bj">BJ</div>
+                    </div>
+                  </div>
+                  <div className="bob">Bob</div>
+                  <div className="male-35">Male - 35 Years</div>
+                  <img
+                    className="group-icon"
+                    alt=""
+                    src="/group-1000000893.svg"
+                  />
+                </div>
+                <div className="group-container">
+                  <div className="group-group">
+                    <img className="group-icon1" alt="" src="/group.svg" />
+                    <div className="fever">Fever</div>
+                  </div>
+                  <div className="group-div">
+                    <img className="group-icon2" alt="" src="/group@2x.png" />
+                    <div className="cough">Cough</div>
+                  </div>
+                  <div className="group-parent1">
+                    <img className="group-icon3" alt="" src="/group.svg" />
+                    <div className="heart-burn">Heart Burn</div>
+                  </div>
+                </div>
+                <div className="group-parent2">
+                  <div className="doctors-contact-parent">
+                    <div className="doctors-contact">Doctors Contact</div>
+                    <div className="dr-everly-everlygmailcom">
+                      Dr Everly (Everly@gmail.com)
+                    </div>
+                  </div>
+                  <div className="high-blood-pressure-parent">
+                    <div className="high-blood-pressure">
+                      High Blood Pressure
+                    </div>
+                    <div className="condition">Condition</div>
+                  </div>
+                  <div className="paracetmol-2-times-a-day-diz-parent">
+                    <div className="paracetmol-2-container">
+                      <span className="nextmedin-txt">
+                        <p className="dizopam-day">
+                          Paracetmol - 2 times a day
+                        </p>
+                        <p className="dizopam-day">
+                          Dizopam - Day and Night before meal
+                        </p>
+                        <p className="dizopam-day">Wikoryl</p>
+                      </span>
+                    </div>
+                    <div className="prescription">Prescription</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="article">
+          <div className="article-child" />
+          <div className="group-parent3">
+            <div className="ellipse-group">
+              <div className="group-child1" />
+              <div className="daily-read">DAILY READ</div>
+            </div>
+            <img
+              className="rectangle-icon"
+              alt=""
+              src="/rectangle-39896@2x.png"
+            />
+            <div className="equitable-medical-education">
+              Equitable medical education with efforts toward real change
+            </div>
+          </div>
+        </div>
+        <div className="calendar">
+          <div className="calendar-child" />
+          <div className="january-2024-parent">
+            <div className="january-2024">January 2024</div>
+            <div className="icons-parent">
+              <img className="icons" alt="" src="/icons.svg" />
+              <img className="icons1" alt="" src="/icons1@2x.png" />
+            </div>
+            <div className="line-div" />
+            <div className="week-base">
+              <div className="sen">Sun</div>
+            </div>
+            <div className="week-base1">
+              <div className="sen">Mon</div>
+            </div>
+            <div className="week-base2">
+              <div className="sen">Tue</div>
+            </div>
+            <div className="week-base3">
+              <div className="sen">Wed</div>
+            </div>
+            <div className="week-base4">
+              <div className="sen">Thu</div>
+            </div>
+            <div className="week-base5">
+              <div className="sen">FrI</div>
+            </div>
+            <div className="week-base6">
+              <div className="sen">SAT</div>
+            </div>
+            <div className="days">
+              <div className="div">4</div>
+            </div>
+            <div className="days1">
+              <div className="div">5</div>
+            </div>
+            <div className="days2">
+              <div className="div">6</div>
+            </div>
+            <div className="days3">
+              <div className="div">7</div>
+            </div>
+            <div className="days4">
+              <div className="div4">1</div>
+            </div>
+            <div className="days5">
+              <div className="div4">2</div>
+            </div>
+            <div className="days6">
+              <div className="div4">3</div>
+            </div>
+            <div className="days7">
+              <div className="div4">4</div>
+            </div>
+            <div className="days8">
+              <div className="div4">5</div>
+            </div>
+            <div className="days9">
+              <div className="div4">6</div>
+            </div>
+            <div className="days10">
+              <div className="div4">7</div>
+            </div>
+            <div className="days11">
+              <div className="div4">8</div>
+            </div>
+            <div className="days12">
+              <div className="div4">9</div>
+            </div>
+            <div className="days13">
+              <div className="div4">10</div>
+            </div>
+            <div className="days14">
+              <div className="div4">11</div>
+            </div>
+            <div className="days15">
+              <div className="div4">12</div>
+            </div>
+            <div className="days16">
+              <div className="div4">13</div>
+            </div>
+            <div className="days17">
+              <div className="div17">14</div>
+              <div className="days-child" />
+              <div className="days-item" />
+            </div>
+            <div className="days18">
+              <div className="div4">15</div>
+            </div>
+            <div className="days19">
+              <div className="div4">16</div>
+            </div>
+            <div className="days20">
+              <div className="div20">17</div>
+            </div>
+            <div className="days21">
+              <div className="div4">18</div>
+            </div>
+            <div className="days22">
+              <div className="div4">19</div>
+            </div>
+            <div className="days23">
+              <div className="div4">20</div>
+            </div>
+            <div className="days24">
+              <div className="div4">21</div>
+            </div>
+            <div className="days25">
+              <div className="div4">22</div>
+            </div>
+            <div className="days26">
+              <div className="div4">23</div>
+            </div>
+            <div className="days27">
+              <div className="div4">24</div>
+            </div>
+            <div className="days28">
+              <div className="div4">25</div>
+            </div>
+            <div className="days29">
+              <div className="div4">26</div>
+            </div>
+            <div className="days30">
+              <div className="div4">27</div>
+            </div>
+            <div className="days31">
+              <div className="div4">28</div>
+            </div>
+            <div className="days32">
+              <div className="div4">29</div>
+            </div>
+            <div className="days33">
+              <div className="div4">30</div>
+            </div>
+            <div className="calendar-parent">
+              <div className="calendar1">Calendar</div>
+              <img className="icons1" alt="" src="/icons2@2x.png" />
+            </div>
+          </div>
+          <div className="frame-parent">
+            <div className="upcoming-parent">
+              <b className="upcoming">Upcoming</b>
+              <div className="view-all">View All</div>
+            </div>
+            <div className="rectangle-div" />
+            <div className="rectangle-group">
+              <div className="group-child2" />
+              <div className="group-parent4">
+                <div className="heading-heading-left-parent">
+                  <div className="heading-heading">Montly doctor’s meet</div>
+                  <div className="heading-heading1">
+                    8 April, 2024 | 04:00 PM
+                  </div>
+                </div>
+                <div className="group-child3" />
+                <b className="m">M</b>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="header">
+          <div className="icons3">
+            <img className="icons-2" alt="" src="/icons21.svg" />
+            <img className="icons-child" alt="" src="/frame-58.svg" />
+          </div>
+          <div className="search-bar">
+            <div className="search-bar-child" />
+            <b className="logo">Pill Pal</b>
+          </div>
+          <div className="profile">
+            <div className="profile-child" />
+            <div className="next-circle">
+              <div className="heading-heading2">Patient</div>
+              <img
+                className="next-circle-child"
+                alt=""
+                src="/ellipse-38@2x.png"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="hello-dr-kim">
+          <div className="intro">
+            <div className="blueback">
+              <div className="blueback-child" />
+              <div className="currentmeds">
+                <div className="current-medication">
+                  <div className="current-medication1">Current Medication</div>
+                </div>
+                <div className="meds">
+                  <div className="med-1">
+                    <div className="med-1-child" />
+                    <div className="vitamin-c">Vitamin C</div>
+                  </div>
+                  <div className="med-2">
+                    <div className="med-1-child" />
+                    <div className="paracetemol">Paracetemol</div>
+                  </div>
+                  <div className="med-3">
+                    <img
+                      className="med-3-child"
+                      alt=""
+                      src="/rectangle-39904.svg"
+                    />
+                    <div className="vitamin-c">Dizopam</div>
+                  </div>
+                </div>
+              </div>
+              <div className="blueback-item" />
+              <div className="nextmedin">
+                <span className="nextmedin-txt">
+                  <p className="dizopam-day">Next Pill in</p>
+                  <p className="dizopam-day">
+                    <span>{` `}</span>
+                    <b className="hr1">3Hr</b>
+                  </p>
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="good-morning-patient-container">
+            <span className="nextmedin-txt">
+              <span>Good Morning</span>
+              <b className="patient">
+                <span className="span">{` `}</span>
+                <span>PATIENT!</span>
+              </b>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardContent;
